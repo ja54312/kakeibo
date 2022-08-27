6cda153176f227cb25c0cd86fc7cf05480d0e3e8
Casuso <albertofcasuso@gmail.com>
2022-08-27 20:31:52 +0200

    firebase configuration files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { UserContext } from "../lib/context";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, userRef,userSnapshot } from "../lib/firebase";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user] = useAuthState(auth);
+  const [username, setUsername] = useState(null);
+
+  useEffect(() => {
+    let unsubscribe;
+    if (user) {
+      console.log(user.uid)
+      const ref = userRef(user.uid);
+      unsubscribe = userSnapshot(ref,(doc)=>{
+        console.log(doc.data())
+      })
+    } else {
+      setUsername(null);
+    }
+    return unsubscribe;
+  }, [user]);
+
+  return (
+    <UserContext.Provider value={{ user, username }}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;

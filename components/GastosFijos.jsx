c93b60e5f136c49c66a9047572fcf40f980aec0e
Casuso <albertofcasuso@gmail.com>
2022-09-01 08:14:01 +0200

    fix al borrar gasto

--- components/GastosFijos.jsx
+++ components/GastosFijos.jsx
@@ -74,7 +74,7 @@ function OperationList(props) {
 
   const handleDelete = (e, item) => {
     e.preventDefault();
-    BorrarGasto(user.uid, item);
+    BorrarGasto(item,user.uid);
   };
 
   const getDate = (date) => new Date(date.date.seconds * 1000).toDateString();

0f68763ee9d45db0e12684df3deba42506c1130b
ja54312 <ija54312@gmail.com>
2022-09-06 16:18:26 -0500

    fix/loader image

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,12 +1,13 @@
 import { SignInWithPopup } from "../lib/firebase"
 import Image from "next/image"
+import LogoGoogle from '../public/google-icon.svg'
 import styles from '../styles/Login.module.css'
 
 export default function Login (){
     const login = () => SignInWithPopup()
     return(
         <div className={styles.containerSocialButton}>
-            <Image src='/google-icon.svg' alt="icono de google" width='40px' height='40px'/>
+            <Image src={LogoGoogle} alt="icono de google" width='40px' height='40px'/>
             <button onClick={login} className={styles.SocialButtonLoginGoogle}>Login with Google</button>
         </div>
     )
--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+//images.path ruta donde se guardaran los archivos ,para produccion cambiar la url de la ubicacion de la url
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    loader: 'imgix',
+    path: 'http://localhost:3000/',
+  },
 }
 
 module.exports = nextConfig
